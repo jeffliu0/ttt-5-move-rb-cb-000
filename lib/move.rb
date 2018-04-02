96fc87fa9f4f05cf7ced34c3c52184315483e886
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-02 22:01:38 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,9 @@ end
 # code your input_to_index and move method here!
 def input_to_index(input)
 
-  if(input.match(/^(\d)+$/))
+  if(!input.match(/^(\d)+$/))
+    return -1
+  end
 
   if input.to_i == 0
     return "0".to_i
